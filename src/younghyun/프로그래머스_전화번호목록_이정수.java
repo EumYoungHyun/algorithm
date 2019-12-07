c6796a97e9d1f9b2ea1128d520918ba09bbbdd32
jsoo0511 <jsoo94@naver.com>
2019-12-07 22:01:58 +0900

    Add files via upload

---
+++ src/younghyun/프로그래머스_전화번호목록_이정수.java
@@ -0,0 +1,34 @@
+package 프로그래머스;
+
+import java.util.Arrays;
+
+public class 프로그래머스_전화번호목록_이정수 {
+
+	public static void main(String[] args) {
+		프로그래머스_전화번호목록_이정수 a = new 프로그래머스_전화번호목록_이정수();
+		String s[]={"119","97674223","1195524421"};
+		System.out.println(a.solution(s));
+
+	}
+
+	 public boolean solution(String[] phone_book) {
+	        boolean answer = true;
+	        Arrays.sort(phone_book);
+	        
+	       outer: for(int i=0;i<phone_book.length;i++){
+	            int len=phone_book[i].length();
+	            for(int j=i+1;j<phone_book.length;j++){
+	                if(len>phone_book[j].length())
+	                    continue;
+	                
+	                String str=phone_book[j].substring(0,len);
+	                if(str.equals(phone_book[i])){
+	                    answer=false;
+	                    break outer;
+	                }
+	            }
+	        }
+	        return answer;
+	    }
+
+}
---
+++ src/younghyun/프로그래머스_탑_이정수.java
@@ -0,0 +1,33 @@
+package 프로그래머스;
+
+import java.util.*;
+
+public class 프로그래머스_탑_이정수 {
+
+	public static void main(String[] args) {
+		프로그래머스_탑_이정수 s = new 프로그래머스_탑_이정수();
+		int arr[] = { 6, 9, 5, 7, 4 };
+		int arr1[] = {3,9,9,3,5,7,2  };
+		int arr2[] = { 1,5,3,6,7,6,5 };
+		System.out.println(Arrays.toString(s.solution(arr)));
+		System.out.println(Arrays.toString(s.solution(arr1)));
+		System.out.println(Arrays.toString(s.solution(arr2)));
+		
+
+	}
+
+	public int[] solution(int[] heights) {
+        int[] answer = new int[heights.length];
+  
+        for(int i=heights.length-1;i>0;i--){
+        	for(int j=i-1;j>=0;j--){
+        		if(heights[i]<heights[j]){
+        			answer[i]=j+1;
+        			break;
+        		}
+        	}
+        }
+        return answer;
+    }
+
+}
