306b250566f693abe1cf31fd900d7f6e9aaa6af3
opwer <opwer@DESKTOP-BASSLCC>
2019-12-07 17:54:34 +0900

    2019-12-07 알고리즘

--- src/younghyun/programmers_전화번호목록_음영현.java
+++ src/younghyun/programmers_전화번호목록_음영현.java
@@ -1,36 +1,22 @@
 package younghyun;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashSet;
 
 public class programmers_전화번호목록_음영현 {
 	public static void main(String[] args) {
-		String[] phone_book = {"123", "2356", "234"};
+		String[] phone_book = {"2343", "23456", "231"};
 		
 		System.out.println(Solution(phone_book));
 	}
 
 	public static boolean Solution(String[] phone_book) {
-		boolean answer = false;
 		
-		//문자열을 짧은 순으로 나열
-		Arrays.sort(phone_book, new Comparator<String>() {
-			@Override
-			public int compare(String o1, String o2) {
-				return o1.length()-o2.length();
+		Arrays.sort(phone_book);
+		System.out.println(Arrays.toString(phone_book));
+		for (int i = 0; i < phone_book.length -1; i++) {
+			if(phone_book[i+1].startsWith(phone_book[i])) {
+				return false;
 			}
-		});
-		HashSet<String> set = new HashSet<String>();
-		
-		for (int i = 0; i < phone_book.length; i++) {
-			int len = phone_book[i].length();
-			for (int j = 1; j < len; j++) {
-				if(set.contains(phone_book[i].substring(0, j))) {
-					return false;
-				}
-			}
-			set.add(phone_book[i]);
 		}
 		
 		return true;
