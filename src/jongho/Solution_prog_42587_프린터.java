6e819ff62b22cdd698dbf704414b2b977e6794a8
JHLee0211 <qpalzmfghtgv@naver.com>
2019-12-09 18:06:39 +0900

    191209 Solution_prog_42586_기능개발 Solution_prog_42587_프린터

---
+++ src/jongho/Solution_prog_42587_프린터.java
@@ -0,0 +1,28 @@
+package jongho;
+import java.util.ArrayList;
+public class Solution_prog_42587_프린터 {
+    public int solution(int[] priorities, int location) {
+		int answer=0;
+		ArrayList<Integer> arr = new ArrayList<>();
+		ArrayList<Integer> arr2 = new ArrayList<>();
+		for(int i=0; i<priorities.length; i++) {
+			arr.add(priorities[i]);
+			arr2.add(i);
+		}
+		int loc = location;
+		int res=-1;
+		while(res!=loc) {
+			int k =arr.remove(0);
+			boolean ch=false;
+			for(int i=0; i<arr.size(); i++) {
+				if(k<arr.get(i)) {
+					arr.add(k); ch=true;
+					arr2.add(arr2.remove(0));
+					break;
+				}
+			}
+			if(!ch) { res=arr2.remove(0); answer++; }
+		}
+		return answer;
+    }
+}
