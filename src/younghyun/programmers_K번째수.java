30dd9d6c833385c9a9960ab3e37a7be6ff521ad8
younghyun <opwer032@naver.com>
2019-12-10 08:47:40 +0900

    19-12-10 알고리즘

---
+++ src/younghyun/programmers_K번째수.java
@@ -0,0 +1,20 @@
+package younghyun;
+
+import java.util.Arrays;
+
+public class programmers_K번째수 {
+	public static void main(String[] args) {
+		int[] array = {1, 5, 2, 6, 3, 7, 4};
+		int[][] commands = {{2, 5, 3},{4, 4, 1},{1, 7, 3}};
+		
+		System.out.println(Arrays.toString(solution(array, commands)));
+	}
+
+	public static int[] solution(int[] array, int[][] commands) {
+		
+		int[] answer = new int[commands.length];
+		
+		
+		return answer;
+	}
+}
