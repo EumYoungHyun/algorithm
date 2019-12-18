b6d1c649bc30568e5f0023d73fcd15f4ee1eee6f
JHLee0211 <qpalzmfghtgv@naver.com>
2019-12-19 01:26:12 +0900

    191219 Solution_prog_43104_타일장식물 Solution_prog_43105_정수삼각형

---
+++ src/jongho/Solution_prog_43104_타일장식물.java
@@ -0,0 +1,20 @@
+package jongho;
+import java.util.*;
+
+//Greedy
+public class Solution_prog_43104_타일장식물 {
+	public static void main(String[] args) {
+		System.out.println(solution(5));
+	}
+    public static long solution(int N) {
+        long answer = 0;
+        long[] len = new long[N];
+        len[0]=1; len[1]=1;
+        for(int i=2; i<N; i++){
+            len[i]=len[i-1]+len[i-2];
+        }
+        answer=len[N-1]*4+len[N-2]*2;
+        
+        return answer;
+    }
+}
