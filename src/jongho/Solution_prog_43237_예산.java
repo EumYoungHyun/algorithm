437a896357c3211a340e0d7894f7d5d1f61348af
JHLee0211 <qpalzmfghtgv@naver.com>
2019-12-24 07:40:48 +0900

    191224 Solution_prog_43237_예산

--- src/jongho/Solution_prog_43237_예산.java
+++ src/jongho/Solution_prog_43237_예산.java
@@ -4,56 +4,38 @@ import java.util.*;
 //DFS,BFS
 public class Solution_prog_43237_예산 {
 	public static void main(String[] args) {
-		String[][] ticket = new String[][] {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
-		System.out.println(Arrays.toString(solution(ticket)));
+		int[] budgets = new int[] {120,110,140,150};
+		System.out.println((solution(budgets,485)));
 	}
-    public static String[] answer;
-    public static String[][] tickets;
-    public static boolean[] visit;
-    public static ArrayList<ArrayList<String>> arr;
-    public static ArrayList<String> a;
-    public static String[] solution(String[][] ticket) {
-        tickets=ticket;
-        visit=new boolean[tickets.length];
-        arr=new ArrayList<>();
-        a=new ArrayList<>();
-        a.add("ICN");
-        dfs("ICN", 0);
-        for(int i=1; i<ticket.length+1; i++){
-            if(arr.size()==1) break;
-            String min = arr.get(0).get(i);
-            for(int j=1; j<arr.size(); j++){
-                if(min.compareTo(arr.get(j).get(i))>0){
-                    min=arr.get(j).get(i);
-                }
-            }
-            for(int j=0; j<arr.size(); j++){
-                if(min.compareTo(arr.get(j).get(i))<0){
-                    arr.remove(j); j--;
-                }
+    public static int solution(int[] budgets, int M) {
+        int answer = 0;
+        int left = 0;
+        int right = 0;
+        for (int budget : budgets) {
+            if (budget > right) {
+                right = budget;
             }
         }
-        answer=new String[arr.get(0).size()];
-        for(int i=0; i<answer.length; i++){
-            answer[i]=arr.get(0).get(i);
+        int middle = 0;
+        while (left <= right) {
+            long sum = 0;
+            middle = (left + right) / 2;
+            for (int budget : budgets) {
+                if (budget >= middle) {
+                    sum += middle;
+                }
+                else {
+                    sum += budget;
+                }
+            }
+            if (sum > M) {
+                right = middle - 1;
+            }
+            else {
+                answer = middle;
+                left = middle + 1;
+            }
         }
         return answer;
-    }
-    public static void dfs(String s, int cnt){
-        if(cnt==tickets.length) {
-            ArrayList<String> newA = new ArrayList<>();
-            newA.addAll(a);
-            arr.add(newA);
-            return;
-        }
-        for(int i=0; i<tickets.length; i++){
-            if(visit[i]==false && tickets[i][0].equals(s)) {
-                visit[i]=true;
-                a.add(tickets[i][1]);
-                dfs(tickets[i][1],cnt+1);
-                a.remove(a.size()-1);
-                visit[i]=false;
-            }
-        }
     }
 }
