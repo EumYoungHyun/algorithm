c689bfb310a0a18d202bea852cae1b081bf2efce
younghyun <opwer032@naver.com>
2019-12-30 10:34:15 +0900

    19-12-30 알고리즘

---
+++ src/younghyun/BOJ_2178_미로탐색.java
@@ -0,0 +1,44 @@
+package younghyun;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class BOJ_2178_미로탐색 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		int[][] map = new int[N][M];
+		for (int i = 0; i < N; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = str.charAt(j)-'0';
+			}
+		}
+		
+		PriorityQueue<Integer[]> queue = new PriorityQueue<Integer[]>(new Comparator<Integer[]>() {
+			@Override
+			public int compare(Integer[] o1, Integer[] o2) {
+				return o1[2] - o2[2];
+			}
+		});
+		queue.add(new Integer[] {0, 0, 1});
+		int[][] check = new int[N][M];
+		int answer = 0;
+		while(queue.isEmpty()) {
+			Integer[] temp = queue.poll();
+			if(temp[0] == N-1 && temp[1] == M-1) {
+				answer = temp[2];
+				return;
+			}
+			
+		}
+		
+	}
+	public static int[][] dirs = {{-1, 0},{0, -1},{1, 0},{0, 1}};
+}
