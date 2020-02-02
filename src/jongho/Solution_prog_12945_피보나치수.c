1d221a358c76bcb0d38706ff72efd3e9f0028a95
JHLee0211 <qpalzmfghtgv@naver.com>
2020-02-02 23:51:09 +0900

    200202 Solution_prog_12945_피보나치수.c

---
+++ src/jongho/Solution_prog_12945_피보나치수.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int s[100001] = {0};
+int solution(int n) {
+    s[0]=0; s[1]=1;
+    return fibo(n);
+}
+
+int fibo(int n){
+    if(n==0 || s[n]!=0) return s[n];
+    else if(n!=0 && s[n]==0) s[n] = (fibo(n-1)+fibo(n-2))%1234567;
+}
